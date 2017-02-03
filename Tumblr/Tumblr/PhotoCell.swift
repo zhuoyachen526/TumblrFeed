850f6144695ca6649c92e4a040735f74350858be
Zhuoya Chen <chen1800@purdue.edu>
2017-02-02 20:49:48 -0500

    Add files via upload

---
+++ Tumblr/Tumblr/PhotoCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by 陈卓娅 on 1/31/17.
+//  Copyright © 2017 Sophia Zhuoya Chen. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var picView: UIImageView!
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var userLabel: UILabel!
+    
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        avatarView.layer.masksToBounds = true
+        avatarView.layer.cornerRadius = self.avatarView.frame.width/2
+        
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
